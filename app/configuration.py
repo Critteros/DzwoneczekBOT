7f4ba2b2ba7af2e1a43e69d4bed19a21ca194b3a
Critteros <szymonkoziol3@gmail.com>
2021-02-23 19:34:54 +0100

    Initial commit

---
+++ app/configuration.py
@@ -0,0 +1,10 @@
+"""
+Managing global-wide app configuration
+"""
+
+
+class Config:
+    """
+    Class that holds app configuration
+    """
+    pass
---
+++ startup.py
@@ -0,0 +1,8 @@
+"""
+This file is the entrypoint of Bot application
+"""
+
+# Library includes
+from pathlib import Path
+
+print('Hello World')
