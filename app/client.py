b141ed7e6b1222694bc1e47a6922dfcc10b32441
Critteros <szymonkoziol3@gmail.com>
2021-02-27 18:04:05 +0100

    Added BotClient class

---
+++ app/client.py
@@ -0,0 +1,75 @@
+
+
+# Lib includes
+from pathlib import Path
+import asyncio
+
+from discord.ext import commands
+import discord
+
+# App includes
+from app.logging.core import Log
+
+
+class BotClient(commands.Bot):
+
+    INSTANCE = None
+
+    def __init__(self, *args, **kwargs) -> None:
+
+        # This class is an singleton so there can bo only one instance
+        if BotClient.INSTANCE is not None:
+            raise RuntimeError(
+                'Called BotClient constructor when instance arleady existed')
+
+        # Read discord token file
+        token_file = Path('.discord')
+
+        if not token_file.exists():
+            raise RuntimeError('Discord token file does not exists')
+
+        with token_file.open(mode='rt', encoding='utf-8') as file:
+            self.token = file.read()
+
+        # Retrive event loop
+        self.loop = asyncio.get_event_loop()
+
+        # Initialize Bot class constructor
+        super().__init__(
+            command_prefix=_get_prefix,
+            *args, **kwargs
+        )
+
+        # To-do load cogs
+
+    async def start(self, *args, **kwargs):
+        """
+        Coroutine that starts the bot client.
+        Shorthand for login() and connect()
+        """
+
+        await self.login(self.token, bot=True)
+        await self.connect(reconnect=True)
+
+    @staticmethod
+    def get_instance():
+
+        if BotClient.INSTANCE is None:
+            raise RuntimeError('There is no client instance')
+        return BotClient.INSTANCE
+
+
+def _get_prefix(bot: BotClient, msg: discord.Message):
+    bot_user_id = bot.user.id
+    prefixes: list = [f'<@!{bot_user_id}> ', f'<@{bot_user_id} ']
+
+    if msg.guild is None:
+        prefixes.append('!')
+        prefixes.append('?')
+        Log.info('Invoked in private channel')
+    else:
+        prefixes.append('!')
+        prefixes.append('?')
+        Log.info('Invoked in serwer')
+
+    return prefixes
--- startup.py
+++ startup.py
@@ -3,11 +3,18 @@ This file is the entrypoint of Bot application
 """
 
 # Library includes
+import asyncio
 
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 # App includes
 import app.configuration as configuration
 from app.logging.core import Log
+from app.client import BotClient
+
+# Just for typing information
+from google.cloud.firestore import Client as FirestoreClient
 
 
 def main() -> None:
@@ -30,6 +37,23 @@ def main() -> None:
 
     for key, value in bot_configuration.__dict__.items():
         Log.warning(f'\t{key}: {value}')
+
+    Log.warning('Initializing firebase')
+    cred = credentials.Certificate('.firebase')
+    firebase_admin.initialize_app(credential=cred)
+
+    # Just testing
+    test_server_id = '792497879175397456'
+    # db: FirestoreClient = firestore.client()
+    # server_stuff = db.collection(f'bot-root/{test_server_id}/server-specific')
+
+    # server_config = server_stuff.document('server-config').get().to_dict()
+
+    # prefix = server_config.get('prefix')
+    # Log.info(f'prefix is {prefix}')
+
+    client = BotClient()
+    asyncio.get_event_loop().run_until_complete(client.start())
 
 
 if __name__ != '__main__':
